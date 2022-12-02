80e5c6b23e4f90fd225cc62e4230b46b98f68879
Clement <Clement@DESKTOP-GROAND5>
2022-12-02 19:11:22 +0800

    Added REST API Endpoints and Unit Of Work pattern

---
+++ CarRentalManagement/Server/Controllers/CustomersController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using CarRentalManagement.Shared.Domain;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CustomersController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        //public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        public async Task<IActionResult> GetCustomers()
+        {
+            //return await _context.Customers.ToListAsync();
+            var Customers = await _unitOfWork.Customers.GetAll();
+            return Ok(Customers);
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}")]
+        //public async Task<ActionResult<Customer>> GetCustomer(int id)
+        public async Task<IActionResult> GetCustomer(int id)
+        {
+            //var Customer = await _context.Customers.FindAsync(id);
+            var Customer = await _unitOfWork.Customers.Get(q => q.id == id);
+
+            if (Customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Customer);
+        }
+
+        // PUT: api/Customers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomer(int id, Customer Customer)
+        {
+            if (id != Customer.id)
+            {
+                return BadRequest();
+            }
+
+            //_context.Entry(Customer).State = EntityState.Modified;
+            _unitOfWork.Customers.Update(Customer);
+
+            try
+            {
+                //await _context.SaveChangesAsync();
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //if (!CustomerExists(id))
+                if (!await CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Customers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer Customer)
+        {
+            await _unitOfWork.Customers.Insert(Customer);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetCustomer", new { id = Customer.id }, Customer);
+        }
+
+        // DELETE: api/Customers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var Customer = await _unitOfWork.Customers.Get(q => q.id == id);
+            if (Customer == null)
+            {
+                return NotFound();
+            }
+
+            await _unitOfWork.Customers.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        private async Task<bool> CustomerExists(int id)
+        {
+            var Customer = await _unitOfWork.Customers.Get(q => q.id == id);
+            return Customer != null;
+        }
+    }
+}
